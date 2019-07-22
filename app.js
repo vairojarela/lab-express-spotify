87904c45aea5f1c2f51d1c7bef9aa4dfd7bdd4b6
vairojarela <vairojarela@gmail.com>
2019-07-22 08:42:03 +0200

    Finish express app

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const hbs = require('hbs');
 
-// require spotify-web-api-node package here:
+ const hbs = require('hbs');
 
-const indexRouter = require('./routes/index');
+ const indexRouter = require('./routes/index');
+const artistRouter = require('./routes/artist');
+const albumsRouter = require('./routes/albums');
+const tracksRouter = require('./routes/tracks');
 
  const app = express();
 
@@ -22,19 +24,20 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
  app.use('/', indexRouter);
+app.use('/artist', artistRouter);
+app.use('/albums', albumsRouter);
+app.use('/tracks', tracksRouter);
 
- // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
 
- // error handler
+
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
+
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
